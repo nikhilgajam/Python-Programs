b83802e587b9f8767f3b9e06b9e8c862c011b44c
Nikhil Gajam <65104684+nikhilgajam@users.noreply.github.com>
2020-05-27 15:58:27 +0530

    Update Pattern printing in python.py

--- Pattern printing in python.py
+++ Pattern printing in python.py
@@ -17,7 +17,7 @@ def another_ra_triangle(num):
         print()
 
 
-def r_triangles_combined(num):
+def ra_triangles_combined(num):
 
     for i in range(1, num+1):           # 1
         for j in range(1, i+1):         # 1 2
@@ -42,11 +42,17 @@ def rl_triangle(num):
             print(j, end=" ")
         print()
 
+        '''
+            1
+          1 2
+        1 2 3
+        '''
+
 
 def another_rl_triangle(num):
     k = 2 * num - 2
 
-    for i in range(num-1, 0, -1):
+    for i in range(num, 0, -1):
         for j in range(k-1, 0, -1):
             print(end=" ")
 
@@ -54,6 +60,42 @@ def another_rl_triangle(num):
         for j in range(1, i+1):
             print(j, end=" ")
         print()
+
+        '''
+        1 2 3
+          1 2
+            1
+        '''
+
+
+def combined_rl_triangle(num):
+    k = num * 2 - 2
+
+    for i in range(1, num+1):
+        for j in range(1, k+1):
+            print(end=" ")
+        k = k - 2
+        for j in range(1, i+1):
+            print(j, end=" ")
+        print()
+
+    k = k + 4  # Important line
+
+    for i in range(num-1, 0, -1):
+        for j in range(1, k+1):
+            print(end=" ")
+        k = k + 2
+        for j in range(1, i+1):
+            print(j, end=" ")
+        print()
+
+        '''
+            1
+          1 2
+        1 2 3
+          1 2
+            1
+        '''
 
 
 def eq_triangle(num):
@@ -79,7 +121,7 @@ def another_eq_triangle(num):
     k = 2 * num - 2
 
     for i in range(num, 0, -1):
-        for j in range(k, 0, -1):
+        for j in range(1, k+1):
             print(end=" ")
 
         k = k + 1
@@ -88,6 +130,36 @@ def another_eq_triangle(num):
         print()
 
         '''
+        1 2 3
+         1 2
+          1
+        '''
+
+
+def combined_eq_triangle(num):
+    k = 2 * num - 2
+
+    for i in range(1, num+1):
+        for j in range(1, k+1):
+            print(end=" ")
+        k = k - 1
+        for j in range(1, i+1):
+            print(j, end=" ")
+        print()
+
+    k = num  # Important line
+
+    for i in range(num-1, 0, -1):
+        for j in range(1, k+1):
+            print(end=" ")
+        k = k + 1
+        for j in range(1, i+1):
+            print(j, end=" ")
+        print()
+
+        '''
+          1
+         1 2
         1 2 3
          1 2
           1
