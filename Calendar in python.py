fdb27674c633dc94f225a6484fa8fd52db720a71
Nikhil Gajam <65104684+nikhilgajam@users.noreply.github.com>
2020-06-15 19:46:01 +0530

    New program added

---
+++ Calendar in python.py
@@ -0,0 +1,10 @@
+import calendar
+# Calendar is inbuilt module
+
+print(calendar.month(2020, 6))  # Prints this months calendar
+
+print(calendar.isleap(2020))    # Prints leap year or not
+
+print(calendar.calendar(2020))  # Prints all months in a year
+
+# It will show the information according to year you entered
