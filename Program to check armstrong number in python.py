805b6b720ffa2af5972cbe1310db5d5064badfa3
Nikhil Gajam <65104684+nikhilgajam@users.noreply.github.com>
2020-08-23 23:04:58 +0530

    Updated

--- Program to check armstrong number in python.py
+++ Program to check armstrong number in python.py
@@ -18,4 +18,29 @@ def arm_rec(num, r=0):
         return r
 
 
+def armstrong_range():
+    print("Armstrong Range Program\n")
+
+    num = int(input("Enter a max range: "))
+    power = int(input("Enter the power\nEx: n^3 is Armstrong\n: "))
+
+    r = 0
+    temp = num
+
+    print("Numbers in range 1 to", num, ": \n")
+
+    for i in range(1, temp+1):
+        num = i
+
+        while num > 0:
+            n = num % 10
+            r = r+(n**power)   # n**3 == n*n*n is Armstrong
+            num //= 10
+
+        if r == i:
+            print(i)
+
+        r = 0
+
+
 print(armst(153))
